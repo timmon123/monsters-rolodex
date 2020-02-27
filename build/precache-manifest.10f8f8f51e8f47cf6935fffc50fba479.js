5b930708bb99ca59c40a9578471302ea5510451f
timmon123 <ttermoellen@googlemail.com>
2020-02-27 13:14:31 +0100

    Adding files for Github Pages

---
+++ build/precache-manifest.10f8f8f51e8f47cf6935fffc50fba479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1f7585c833cc99cc075ea305440455",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e4d576e8726bf083b8a",
+    "url": "/monsters-rolodex/static/css/main.dcdd8005.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4d576e8726bf083b8a",
+    "url": "/monsters-rolodex/static/js/main.779ffbba.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
